6ff8a9dbea1bc263d438c948ba34069da4ef0867
Robert Glas <robert.glas@vodafone.com>
2016-08-22 16:20:30 +0200

    initial

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,10 @@
+Given(/^Enter search term 'Cucumber'$/) do
+end
+
+When(/^Do search$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^Single result is shown for 'Cucumber'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
